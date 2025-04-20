01101820ca8dabe8969b4f59752d064b0a8db61c
Cedric <cedric.claire03@gmail.com>
2025-04-20 14:53:10 +0200

    Account

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/lacarte", "La carte", "/pages/lacarte.html"),
-    new Route("/signin", "Connexion", "pages/signin.html"),
-    new Route("/signup", "Inscription", "pages/signup.html"),];
+    new Route("/signin", "Connexion", "pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "pages/auth/signup.html"),
+    new Route("/account", "Mon Compte", "pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "pages/auth/editPassword.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
